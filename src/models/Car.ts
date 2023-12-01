2e41ece081ef14d694155dceebc6bac901b83c40
janzen0907 <janzen0907@saskpolytech.ca>
2023-12-01 10:20:28 -0600

    Added models for car and trader --Jonh

---
+++ src/models/Car.ts
@@ -0,0 +1,48 @@
+import {IsCurrency, IsNumber, IsOptional, Length, Max, MaxLength, min, Min} from "class-validator";
+
+
+export default class Car {
+
+  @IsOptional()
+  id!: number|undefined;
+
+  //NOTE: We could change this to a select box of available care makes if time permits
+  @Length(1, 50, {
+    message: "Car Make must be from $constraint1 to $constraint2 charecters",
+  })
+  make!: string;
+
+  @Length(1, 50, {
+    message: "Car Make must be from $constraint1 to $constraint2 charecters",
+})
+  model!: string;
+
+  @Min(1908, {
+    message: "Car year must be newer than 1908"
+  })
+  @Max(2024, {
+    message: "Car year cannot be newer than 2024"
+  })
+  year!: number
+
+  @IsNumber()
+  km!:  number
+
+  @IsCurrency({},  {message: "Must be Canadian currency"})
+  price!: number
+
+  @Length(1, 20, {
+    message: "Transmission should be either Manual or Automatic"
+  })
+  transmission!: string
+
+  @MaxLength(3, {message: " Drivetrain should either be FWD, RWD or AWD only"})
+  drivetrain!: string
+
+  @IsOptional()
+  numUpvotes!: number
+
+  @IsOptional()
+  numDownVotes!: number
+
+}
---
+++ src/models/Trader.ts
@@ -0,0 +1,21 @@
+import {IsEmail, IsOptional, Length} from "class-validator";
+
+
+export default class Trader{
+  @IsOptional()
+  id!: number|undefined;
+
+  @IsEmail({}, {message: "Must be valid email"})
+  email!: string
+
+  @Length(1, 300, {
+    message: "Name must be between $constrain1 and $constraint2 charecters"
+  })
+  name!: string
+
+  @IsOptional()
+  rating!: number
+
+  @IsOptional()
+  ratingCount!: number
+}
