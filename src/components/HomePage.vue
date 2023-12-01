5fb5fa7c5e7103dc51e4e42ce42f6f2ee2a31ec9
cheezbrik <147870779+cheezbrik@users.noreply.github.com>
2023-12-01 11:49:35 -0600

    started homepage

---
+++ src/components/HomePage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello" align="center">
+    <h1>{{ msg }}</h1>
+    <p>
+      I hope all you guys love cars just as much as we do.
+    </p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component({
+  components: {},
+})
+export default class HomePage extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
