4fc9d64f74013b7a85e8dc55c5c1eb4218ed4fee
janzen0907 <janzen0907@saskpolytech.ca>
2023-12-04 13:53:54 -0600

    Everything is broken, trying to get car infomration to work

--- src/views/BrowseVehiclesView.vue
+++ src/views/BrowseVehiclesView.vue
@@ -1,6 +1,7 @@
 <script lang="ts">
 import { Vue, Component, Mixins } from 'vue-property-decorator';
 import { BvTableCtxObject } from 'bootstrap-vue/src/components/table';
+import { BCard } from 'bootstrap-vue';
 import BrowseVehiclesForm from '@/components/BrowseVehiclesForm.vue';
 import GlobalMixin from '@/mixins/global-mixin';
 
@@ -8,13 +9,32 @@ import GlobalMixin from '@/mixins/global-mixin';
   components: { BrowseVehiclesForm },
 })
 export default class BrowseVehicles extends Mixins(GlobalMixin) {
+  $refs!: {
+    carCard: BCard;
+  };
+
   // any logic for the card would go here
   // TODO: Finish working on this API call currently is not workig
-  provider(ctx:BvTableCtxObject):Promise<any> {
+  provider(ctx: BvTableCtxObject): Promise<any> {
     return this.callAPI(`${ctx.apiUrl}`);
   }
 
-  fields=[]
+  get cars() {
+    return this.$refs.carCard.localItems;
+  }
+
+  fields = [
+    { key: 'id', sortable: true },
+    { key: 'make', sortable: true },
+    { key: 'model', sortable: true },
+    { key: 'year', sortable: true },
+    { key: 'km', sortable: true },
+    { key: 'price', sortable: true },
+    { key: 'transmission', sortable: true },
+    { key: 'drivetrain', sortable: true },
+    { key: 'numUpVotes', sortable: true },
+    { key: 'numDownVotes', sortable: true },
+  ]
 }
 </script>
 
@@ -24,8 +44,11 @@ export default class BrowseVehicles extends Mixins(GlobalMixin) {
       <!--      TODO: Add Car form here once completed-->
       <browse-vehicles-form>
         <!--        Emit needed events for the buttons-->
-        <b-form
-          :api-url="CAR_API"/>
+        <b-card
+          ref="carCard"
+          :fields="fields"
+          api-url="http://localhost:3000/cars"
+        />
       </browse-vehicles-form>
     </b-card>
   </div>
