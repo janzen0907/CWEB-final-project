a966a25077dd6634f2814fbcdc87ca29d18785b3
janzen0907 <janzen0907@saskpolytech.ca>
2023-12-05 11:44:22 -0600

    Finished formating for browse vehicles page. Still need seller data and logic --JohnJ

--- src/components/BrowseVehiclesForm.vue
+++ src/components/BrowseVehiclesForm.vue
@@ -43,12 +43,47 @@ export default class BrowseVehicles extends Vue {
             <p>Price: {{ carLoop.price }}</p>
             <p>Transmission: {{ carLoop.transmission }}</p>
             <p>Drivetrain: {{ carLoop.drivetrain }}</p>
+            <!--            Can use float right to move it but then its on the same line as seller stuff-->
+            <b-row>
+              <b-col>
+                <b-button variant="success" size="lg" class="mb-3">Rate this seller</b-button>
+              </b-col>
+            </b-row>
+            <b-row>
+              <b-col cols="6">
+                <!--                Add the seller name from the db here-->
+                <!--                Need to add logic here probably a V-IF. Show edit listing if the -->
+                <!--                car belongs to that person otherwise it can show the edit and rating buttons-->
+                <p>Seller Name:</p>
+
+              </b-col>
+              <b-col cols="6">
+                <label for="rating-inline">Seller Rating:</label>
+                <!--                TODO: Get rating from DB per seller-->
+                <b-form-rating id="rating-inline" inline value="4" :readonly="true"/>
+              </b-col>
+            </b-row>
+            <b-row>
+              <b-col cols="6">
+                <b-button class="m-3">
+                  <b-icon icon="hand-thumbs-up"/>
+                  <!--                  Insert number of upvotes here-->
+                </b-button>
+              </b-col>
+              <b-col cols="6">
+                <b-button class="m-3">
+                  <b-icon icon="hand-thumbs-down"/>
+                </b-button>
+              </b-col>
+            </b-row>
           </b-card-text>
           <!--    TODO: Add seller information, buttons and upvotes and downvotes-->
           <!--          Going to need another loop for seller information -->
         </b-card>
       </b-col>
     </b-row>
+    <!--    hand-thumbs-up-->
+    <!--hand-thumbs-down-->
   </div>
 </template>
 <style scoped/>
